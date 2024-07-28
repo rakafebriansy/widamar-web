6f314431d54be4243bdc5f52ccd1017a8e5a3c31
rakafebriansy <kerenr445@gmail.com>
2024-07-28 12:51:30 +0700

    feat: hero, wisata, tentang kami, and peta wisata on Landing Page

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,60 @@
+import Navbar from "../components/Navbar"
+import HeroImg from "../assets/Hero.png"
+import SlideImg from "../assets/Slide.png"
+import NoodleImg from "../assets/Noodle.png"
+import RiverImg from "../assets/River.png"
+import QRImg from "../assets/QR.png"
+import logo from "../assets/Logo.png"
+import RectangleCard from "../components/RectangleCard"
+
+const LandingPage = () => {
+    return (
+        <div className="relative w-screen h-screen bg-gradient-to-b from-[rgba(41,64,38,1)] via-[rgba(41,64,38,0.5)] via-10% to-20% to-[rgba(0,0,0,0)]">
+            <Navbar/>
+            <section id="home" className="relative min-h-screen min-w-full">
+                <img className="absolute top-0 left-0 w-full h-full z-[-1]" src={HeroImg} alt="Gambar orang berenang" />
+                <div className="h-[4rem] w-full bg-gradient-to-t from-white from-40% to-[rgba(0,0,0,0)] absolute bottom-0"></div>
+            </section>
+            <section className="bg-white flex flex-col items-center pt-[7rem] pb-[4rem]">
+                <div className="w-[80%] flex flex-col gap-[4rem] items-center">
+                    <h1 className="text-4xl font-bold">Wisata</h1>
+                    <div className="flex w-full justify-between gap-14">
+                        <RectangleCard title="Mini Tubing" image={RiverImg}>We book all kind of national or international ticket for your destinaion.</RectangleCard>
+                        <RectangleCard title="Outbond" image={SlideImg}>You can easily book your according to your budget hotel by our website.</RectangleCard>
+                        <RectangleCard title="Wisata Kuliner" image={NoodleImg}>We provide you the best plan within a short time explore more.</RectangleCard>
+                    </div>
+                </div>
+            </section>
+            <section id="tentang-kami" className="bg-[#F2F6F6] flex justify-center w-full relative">
+                <img src={logo} className="opacity-40 absolute z-[1] w-[42rem] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
+                <div className="w-[50%] pt-10 flex flex-col justify-between pb-32 text-center gap-32">
+                    <h1 className="text-4xl font-bold">Tentang Kami</h1>
+                    <div className="text-3xl font-semibold text-justify tracking-tight">
+                        <p>WIDAMAR merupakan proyek yang bertujuan untuk mentransformasi Desa Ajung menjadi destinasi ekowisata digital yang menarik. Proyek ini melibatkan pengembangan wisata interaktif di sekitar dua dam kembar yang ada di wilayah tersebut, dengan implementasi teknologi Augmented Reality (AR) untuk meningkatkan pengalaman wisatawan. Melalui pemanfaatan teknologi AR, pengunjung dapat berinteraksi dengan lingkungan sekitar, mendapatkan informasi tambahan, serta visualisasi digital yang memperkaya pengalaman mereka.</p>
+                    </div>
+                </div>
+            </section>
+            <section className="flex justify-center p-10">
+                <div className="w-[70%] flex flex-col gap-10">
+                    <div className="text-[#646464] flex flex-col items-center gap-2 w-full">
+                        <h1 className="text-4xl font-bold">Maps dan Peta Wisata</h1>
+                        <p className="text-2xl">Easy and Fast</p>
+                    </div>
+                    <div className="grid grid-cols-2">
+                        <div className="flex justify-center items-center">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3949.786605865273!2d113.83518407505568!3d-8.123196491906326!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd6bff1e82a8ac3%3A0x2c242e9b954f82f!2sDam%20kembar!5e0!3m2!1sid!2sid!4v1722145332836!5m2!1sid!2sid" className="w-[35rem] h-[27rem]" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
+                        <div className="flex justify-between flex-col gap-1 items-center">
+                            <div className="font-bold bg-[#285B0A] w-fit py-3 px-5 rounded-2xl text-white text-2xl">
+                                <p>Scan Untuk Menjelajah</p>
+                            </div>
+                            <img src={QRImg} className="w-[23rem]" />
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default LandingPage
